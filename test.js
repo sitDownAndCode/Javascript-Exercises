34556306069200e5a40ddb428f779490ab37bf5a
sitDownAndCode <achbrt01@gmail.com>
2022-06-16 18:31:30 +0545

    Do first exercise of js.

---
+++ test.js
@@ -0,0 +1,26 @@
+console.log("Hello Canada!!!");
+
+const lotsOfDecimal = 3.14159265358979323;
+lotsOfDecimal;
+const twoDecimalPlaces = lotsOfDecimal.toFixed(5);
+twoDecimalPlaces;
+
+console.log(twoDecimalPlaces);
+
+let a = 3;
+let b = 5;
+console.log(0 + a + b + 9 + 5 - 12);
+
+console.log((4 + 6 + 9) / 77);
+console.log(5 * a + b);
+let c = 5;
+b = 7 * c;
+console.log(b);
+
+const max = 57;
+let num = max - 13;
+let percentage = num / max;
+console.log(percentage);
+
+let d = 6 * a;
+console.log(d);
